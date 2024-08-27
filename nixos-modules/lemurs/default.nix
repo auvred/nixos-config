430fa87c9744cc9bce8960b1c0e20938e9e607f5
auvred <aauvred@gmail.com>
2024-08-27 17:28:45 +0300

    bump nixpkgs

--- nixos-modules/lemurs/default.nix
+++ nixos-modules/lemurs/default.nix
@@ -80,41 +80,63 @@ in {
       password include login
     '';
     # See https://github.com/coastalwhite/lemurs/issues/166
-    login.setLoginUid = false;
-  };
-
-  # I want my systemd-logind(8) session to access to seat0 (the default seat
-  # with all hardware devices).
-  # At the very least, I want to be able to change backlight without tweaking
-  # udev rules and directly accessing /sys/class/backlight.
-  # See systemd-logind(8), sd-login(3), org.freedesktop.login1(5), pam_systemd(8)
-  # for more details.
-  #
-  # -----
-  #
-  # pam_systemd(8) tries to get XDG_SEAT and XDG_VTNR variables from environment.
-  # https://github.com/systemd/systemd/blob/main/src/login/pam_systemd.c#L967-L968
-  #
-  # It passes seat and vntr to the
-  # org.freedesktop.login1
-  #   -> /org/freedesktop/login1
-  #      -> org.freedesktop.login1.Manager
-  #         -> CreateSession(in s seat_id, in u vtnr, ...)
-  #
-  # See org.freedesktop.login1(5) for more details.
-  #
-  # AFAIK lemurs doesn't pass either XDG_SEAT or XDG_VTNR to the PAM process.
-  # See also https://github.com/1wilkens/pam/issues/13.
-  #
-  # pam_env(8) PAM module is located before pam_systemd(8) in /etc/pam.d/login.
-  # So variables set by pam_env are visible to pam_systemd.
-  # environment.sessionVariables are written to /etc/pam/environment and then
-  # read by pam_systemd.
-  environment.sessionVariables = {
-    XDG_SEAT = "seat0";
-    XDG_VTNR = "${toString virtualTerminalNumber}";
+    login = {
+      setLoginUid = false;
+      # I want my systemd-logind(8) session to access to seat0 (the default seat
+      # with all hardware devices).
+      # At the very least, I want to be able to change backlight without tweaking
+      # udev rules and directly accessing /sys/class/backlight.
+      # See systemd-logind(8), sd-login(3), org.freedesktop.login1(5), pam_systemd(8)
+      # for more details.
+      #
+      # -----
+      #
+      # pam_systemd(8) tries to get XDG_SEAT and XDG_VTNR variables from environment.
+      # https://github.com/systemd/systemd/blob/main/src/login/pam_systemd.c#L967-L968
+      #
+      # It passes seat and vntr to the
+      # org.freedesktop.login1
+      #   -> /org/freedesktop/login1
+      #      -> org.freedesktop.login1.Manager
+      #         -> CreateSession(in s seat_id, in u vtnr, ...)
+      #
+      # See org.freedesktop.login1(5) for more details.
+      #
+      # AFAIK lemurs doesn't pass either XDG_SEAT or XDG_VTNR to the PAM process.
+      # See also https://github.com/1wilkens/pam/issues/13.
+      #
+      # pam_env(8) PAM module is located before pam_systemd(8) in /etc/pam.d/login.
+      # So variables set by pam_env are visible to pam_systemd.
+      rules.session.extra-env = {
+        args = [
+          ("conffile="
+            + (pkgs.writeTextFile {
+              name = "pam-extra-env-conffile";
+              text = ''
+                XDG_SEAT   DEFAULT="seat0"
+                XDG_VTNR   DEFAULT="2"
+              '';
+            }))
+          "readenv=0"
+        ];
+        control = "required";
+        enable = true;
+        modulePath = "${pkgs.pam}/lib/security/pam_env.so";
+        order = config.security.pam.services.login.rules.session.env.order + 10;
+      };
+    };
   };
 
   # Useful for pam_systemd debugging
+  # systemd.managerEnvironment = {SYSTEMD_LOG_LEVEL = "debug";};
+  # systemd.globalEnvironment = {
+  #   SYSTEMD_LOG_LEVEL = "debug";
+  # };
+  # systemd.services.systemd-logind.environment = {
+  #   SYSTEMD_LOG_LEVEL = "debug";
+  # };
+  # security.pam.services.login.rules.session.unix.args = ["debug"];
   # security.pam.services.login.rules.session.systemd.args = ["debug"];
+  # security.pam.services.systemd-user.rules.session.unix.args = ["debug"];
+  # security.pam.services.systemd-user.rules.session.systemd.args = ["debug"];
 }
